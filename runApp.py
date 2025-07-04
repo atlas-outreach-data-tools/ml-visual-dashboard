b7d8c5ed5f54b711febdc91000f9f995f43b6793
Caley Luce Yardley <caley.luce.yardley@cern.ch>
2025-07-04 15:37:19 +0100

    Add MathJax CDN

--- runApp.py
+++ runApp.py
@@ -16,7 +16,21 @@ if not FilesExist:
 from source.DashComponents import *
 from assets.text import PageHeaders, PageText
 
-NN = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+##USE MATHJAX CDN FOR HANDLING OF LATEX IN PLOTS
+
+MATHJAX_CDN = '''
+#https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/
+#MathJax.js?config=TeX-MML-AM_CHTML'''
+
+external_scripts = [
+                    {'type': 'text/javascript',
+                     'id': 'MathJax-script',
+                     'src': MATHJAX_CDN,
+                     },
+                    ]
+
+
+NN = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP], external_scripts=external_scripts)
 
 ATLASLogo = dbc.Card(
     [
