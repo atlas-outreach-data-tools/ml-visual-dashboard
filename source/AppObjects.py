7086bdc7aa9886e8196d2c5e7f43bc9996b05274
Caley Yardley <127116148+cly20@users.noreply.github.com>
2025-07-11 16:37:35 +0100

    Update AppObjects.py
    Remove double brackets in LaTeX

--- source/AppObjects.py
+++ source/AppObjects.py
@@ -32,12 +32,12 @@ class UIObjects:
         self.Features = DB.df_scatter.drop(columns=['Event','totalWeight']).columns.to_list()
         self.featX = 'ETmiss'
 
-        self.LaTeXDict = {'ETmiss' : r"$E_{T}^{\text{miss}}~\text{[GeV]}$",
+        self.LaTeXDict = {'ETmiss' : r"$E_{T}^{\text{miss}}~\text{GeV}$",
                             'ETmiss_over_HT' : r"$E_{T}^{\text{miss}}/H_{T}$",
                             'sum_lep_charge' : r"$\Sigma{}Q_{lepton}$",
-                            'lead_lep_pt' : r"$p_{T}^{\text{lep} 1}~\text{[GeV]}$",
-                            'sublead_lep_pt' : r"$p_{T}^{\text{lep} 2}~\text{[GeV]}$",
-                            'mll' : r"$m_{ll}~\text{[GeV]}$",
+                            'lead_lep_pt' : r"$p_{T}^{\text{lep} 1}~\text{GeV}$",
+                            'sublead_lep_pt' : r"$p_{T}^{\text{lep} 2}~\text{GeV}$",
+                            'mll' : r"$m_{ll}~\text{GeV}$",
                             'dRll' : r"$\Delta{}R(l,l)$",
                             'dphi_pTll_ETmiss' : r"$\Delta{}R(p_{T}^{ll}, E_{T}^{\text{miss}})$",
                             'fractional_pT_difference' : r"Fractional $p_{T}$ difference", 
@@ -45,7 +45,7 @@ class UIObjects:
                             'WZ' : r"$WZ+$jets",
                             'Z+jets' : r"$Z+$jets",
                             'Non-resonant_ll' : r"Non-resonant $ll$",
-                            'DM_300' : r"DM $M=300~\text{[GeV]}$"
+                            'DM_300' : r"DM $M=300~\text{GeV}$"
                         }
         self.inLaTeXDict = {}
         for key, value in self.LaTeXDict.items():
